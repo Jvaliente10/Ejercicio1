8dc7b8ba71a64e9866e2b159b8436d2d90b500bf
jose antonio <leonidas600@gmail.com>
2022-10-17 20:07:56 +0200

    a medias

--- app/src/main/java/com/example/joseantoniovaliente/ejercicio1/Person.kt
+++
@@ -1,7 +0,0 @@
-package com.example.joseantoniovaliente.ejercicio1
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
- data class Person(val name: String, val mail: String, val phone: String, val image: String):Parcelable
